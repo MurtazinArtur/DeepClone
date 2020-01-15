f6a6492f4267481218acb989aebd3751a4508301
Murtazin_Artur <arturmurtazin85@gmail.com>
2020-01-16 02:58:42 +0300

    Added tests deepCopy Object Collection and Array Type

--- src/main/java/newjob/deepclone/CopyUtils.java
+++ src/main/java/newjob/deepclone/CopyUtils.java
@@ -6,6 +6,7 @@ import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -17,34 +18,113 @@ public class CopyUtils {
         if (isNull.test(originalObject)) {
             throw new IllegalArgumentException("Original object must be not null for Deep Copy");
         }
+        if (originalObject.getClass().isArray()) {
+            return getCloneObjectOfArrayType(originalObject);
+        }else if (Collection.class.isAssignableFrom(originalObject.getClass())) {
+            return getCloneObjectOfCollectionType(originalObject);
+        }else if (Map.class.isAssignableFrom(originalObject.getClass())){
+            return getCloneObjectOfMapType(originalObject);
+        }
 
-        Field[] originalObjectFields = originalObject.getClass().getDeclaredFields();
-        Object deepCopyObject = createNewObject(originalObject);
-        Stream<Field> fieldStream = Arrays.stream(originalObjectFields);
-        Predicate<Field> isPrimitive = field -> field.getType().isPrimitive();
-        Predicate<Field> isString = field -> "String".equals(field.getType().getSimpleName());
-        Predicate<Field> isCollection = field -> Collection.class.isAssignableFrom(field.getType());
-        Predicate<Field> isArray = field -> field.getType().isArray();
-        Predicate<Field> isEnum = field -> field.getType().isEnum();
+            Field[] originalObjectFields = originalObject.getClass().getDeclaredFields();
+            Object deepCopyObjectWithFields = createNewObject(originalObject);
+            Stream<Field> fieldStream = Arrays.stream(originalObjectFields);
+            Predicate<Field> isPrimitive = field -> field.getType().isPrimitive();
+            Predicate<Field> isString = field -> "String".equals(field.getType().getSimpleName());
+            Predicate<Field> isCollection = field -> Collection.class.isAssignableFrom(field.getType());
+            Predicate<Field> isArray = field -> field.getType().isArray();
+            Predicate<Field> isEnum = field -> field.getType().isEnum();
 
-        fieldStream.forEach(field -> {
-            field.setAccessible(true);
-            if (!isNull.test(deepCopyObject)) {
-                if (isNull.test(getFieldValue(field, originalObject))) {
-                    insertValue(deepCopyObject, field, null);
-                } else if (isString.or(isPrimitive).or(isEnum).test(field)) {
-                    cloneObjectFieldPrimitiveOrStringType(originalObject, deepCopyObject, field);
-                } else if (isCollection.test(field)) {
-                    cloneObjectFieldCollectionType(originalObject, deepCopyObject, field);
-                } else if (isArray.test(field)) {
-                    cloneObjectFieldArrayType(originalObject, deepCopyObject, field);
-                } else {
-                    cloneObjectFieldObjectType(originalObject, deepCopyObject, field);
+            fieldStream.forEach(field -> {
+                field.setAccessible(true);
+                if (!isNull.test(deepCopyObjectWithFields)) {
+                    if (isNull.test(getFieldValue(field, originalObject))) {
+                        insertValue(deepCopyObjectWithFields, field, null);
+                    } else if (isString.or(isPrimitive).or(isEnum).test(field)) {
+                        cloneObjectFieldPrimitiveOrStringType(originalObject, deepCopyObjectWithFields, field);
+                    } else if (isCollection.test(field)) {
+                        cloneObjectFieldCollectionType(originalObject, deepCopyObjectWithFields, field);
+                    } else if (isArray.test(field)) {
+                        cloneObjectFieldArrayType(originalObject, deepCopyObjectWithFields, field);
+                    } else {
+                        cloneObjectFieldObjectType(originalObject, deepCopyObjectWithFields, field);
+                    }
+                }
+            });
+            return deepCopyObjectWithFields;
+
+    }
+
+    private Object getCloneObjectOfMapType(Object original) {
+        Map<Object, Object> subObject = (Map<Object, Object>) original;
+        Map<Object, Object>copyCollection = (Map<Object, Object>) createNewObject(subObject);
+
+       // subObject.forEach(getKeyFromValue());
+        
+        return null;
+    }
+    private static Object getKeyFromValue(Map hm, Object value) {
+        for (Object o : hm.keySet()) {
+            if (hm.get(o).equals(value)) {
+                return o;
+            }
+        }
+        return null;
+    }
+
+    private Object getCloneObjectOfArrayType(Object original) {
+        Object copyObject = null;
+        int arrayLength = Array.getLength(original);
+        String theClassName = original.getClass().getComponentType().getTypeName();
+
+        try {
+            Class arrayClass = getArrayClass(theClassName);
+
+            copyObject = Array.newInstance(arrayClass, arrayLength);
+
+            if ("String".equals(arrayClass.getSimpleName()) || arrayClass.isPrimitive()) {
+                System.arraycopy(original, 0, copyObject, 0, arrayLength);
+            } else {
+
+                for (int index = 0; index < arrayLength; index++) {
+                    Array.set(copyObject, index, deepCopy(Array.get(original, index)));
                 }
             }
-        });
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return copyObject;
+    }
 
-        return deepCopyObject;
+    private Object getCloneObjectOfCollectionType(Object original){
+        try {
+            Collection<Object> subObject = (Collection<Object>) original;
+            Collection<Object> copyCollection = (Collection<Object>) createNewObject(subObject);
+            Object collectionItem = subObject.iterator().next();
+            String collectionType = collectionItem.getClass().getTypeName();
+            Class collectionClass = getObjectClass(collectionType);
+
+            if ("String".equals(collectionItem.getClass().getSimpleName()) || collectionClass.isPrimitive()) {
+                assert !isNull.test(copyCollection);
+
+                copyCollection.addAll(subObject);
+
+                return copyCollection;
+            } else {
+
+                for (int i = 0; i < subObject.size(); i++) {
+                    if (subObject.iterator().hasNext()) {
+
+                        assert !isNull.test(copyCollection);
+                        copyCollection.add(deepCopy(subObject.iterator().next()));
+                    }
+                }
+                return copyCollection;
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void cloneObjectFieldPrimitiveOrStringType(Object original, Object clone, Field cloningField) {
@@ -52,24 +132,32 @@ public class CopyUtils {
     }
 
     private void cloneObjectFieldCollectionType(Object original, Object clone, Field cloningField) {
-        Collection<Object> subObject = (Collection<Object>) getFieldValue(cloningField, original);
-        Collection<Object> copyCollection = (Collection<Object>) createNewObject(subObject);
-        Object collectionItem = subObject.iterator().next();
+        try {
+            Collection<Object> subObject = (Collection<Object>) getFieldValue(cloningField, original);
+            Collection<Object> copyCollection = (Collection<Object>) createNewObject(subObject);
+            Object collectionItem = subObject.iterator().next();
+            String collectionType = collectionItem.getClass().getTypeName();
+            Class collectionClass = getObjectClass(collectionType);
 
-        if ("String".equals(collectionItem.getClass().getSimpleName())) {
-            assert !isNull.test(copyCollection);
-            copyCollection.addAll(subObject);
-        } else {
-
-            for (int i = 0; i < subObject.size(); i++) {
-                Object collectionObjectItem = subObject.iterator().next();
-
+            if ("String".equals(collectionItem.getClass().getSimpleName()) || collectionClass.isPrimitive()) {
                 assert !isNull.test(copyCollection);
-                copyCollection.add(deepCopy(collectionObjectItem));
-            }
-        }
+                copyCollection.addAll(subObject);
 
-        insertValue(clone, cloningField, copyCollection);
+                insertValue(clone, cloningField, copyCollection);
+            } else {
+
+                for (int i = 0; i < subObject.size(); i++) {
+                    Object collectionObjectItem = subObject.iterator().next();
+
+                    assert !isNull.test(copyCollection);
+                    copyCollection.add(deepCopy(collectionObjectItem));
+
+                }
+                insertValue(clone, cloningField, copyCollection);
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     private void cloneObjectFieldObjectType(Object original, Object clone, Field cloningField) {
@@ -83,8 +171,8 @@ public class CopyUtils {
         try {
             Object subObject = getFieldValue(cloningField, original);
             int arrayLength = Array.getLength(subObject);
-            String theClassName = subObject.getClass().getComponentType().getTypeName();
-            Class arrayClass = getArrayClass(theClassName);
+            String arrayType = subObject.getClass().getComponentType().getTypeName();
+            Class arrayClass = getArrayClass(arrayType);
             Object copySubObject = Array.newInstance(arrayClass, arrayLength);
 
             if ("String".equals(arrayClass.getSimpleName()) || arrayClass.isPrimitive()) {
@@ -156,6 +244,27 @@ public class CopyUtils {
             case "float":
                 return float.class;
             case "char":
+                return char.class;
+            default:
+                return Class.forName(className);
+        }
+    }
+
+    private Class getObjectClass(String className) throws ClassNotFoundException {
+        switch (className) {
+            case "java.lang.Byte":
+                return byte.class;
+            case "java.lang.Short":
+                return short.class;
+            case "java.lang.Integer":
+                return int.class;
+            case "java.lang.Long":
+                return long.class;
+            case "java.lang.Double":
+                return double.class;
+            case "java.lang.Float":
+                return float.class;
+            case "java.lang.Character":
                 return char.class;
             default:
                 return Class.forName(className);
--- src/test/java/newjob/deepclone/CopyUtilsTest.java
+++ src/test/java/newjob/deepclone/CopyUtilsTest.java
@@ -5,31 +5,30 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CopyUtilsTest {
     Man originalMan;
-    ObjectWithArrays original;
+    ObjectWithArrays originalArray;
     CopyUtils copyUtils;
 
     @BeforeEach
     void setUp() {
         copyUtils = new CopyUtils();
 
-        List<String> favoriteBooks = new ArrayList<>();
-        favoriteBooks.add("How I do it?");
-        favoriteBooks.add("It is working");
-        originalMan = new Man("Zeus", 25, Sex.FEMALE, favoriteBooks);
+        List<String> originalList = new ArrayList<>();
+        originalList.add("How I do it?");
+        originalList.add("It is working");
+        originalMan = new Man("Zeus", 25, Sex.FEMALE, originalList);
 
-        Man manTwo = new Man("Ares", 28, Sex.MALE, favoriteBooks);
+        Man manTwo = new Man("Ares", 28, Sex.MALE, originalList);
         int[] ints = {1, 2, 3};
         String[] strings = {"Земля", "Вода"};
         Man[] mans = {originalMan, manTwo};
-        original = new ObjectWithArrays(ints, strings, mans);
+        originalArray = new ObjectWithArrays(ints, strings, mans);
     }
 
     @AfterEach
@@ -38,53 +37,166 @@ class CopyUtilsTest {
     }
 
     @Test
+    @DisplayName("Test Deep Copy Array Of Primitives")
+    void deepCopyArrayOfPrimitivesTest(){
+        int[] ints = {1, 2, 3};
+
+        int[] copy = (int[]) copyUtils.deepCopy(ints);
+
+        ints = new int[]{4, 5, 6};
+
+        assertNotEquals(ints, copy);
+
+        System.out.println("Original array " + Arrays.toString(ints));
+        System.out.println("Deep Copy array " + Arrays.toString(copy));
+    }
+
+    @Test
+    @DisplayName("Test Deep Copy Array Of Strings")
+    void deepCopyArrayOfStringsTest(){
+        String[] strings = {"Земля", "Вода"};
+
+        String[] copy = (String[]) copyUtils.deepCopy(strings);
+
+        strings[0] = "Воздух";
+        strings[1] = "Огонь";
+
+        assertNotEquals(strings, copy);
+
+        System.out.println("Original array " + Arrays.toString(strings));
+        System.out.println("Deep Copy array " + Arrays.toString(copy));
+    }
+
+    @Test
+    @DisplayName("Test Deep Copy Array Of Objects")
+    void deepCopyArrayOfObjectsTest(){
+        Man manTwo = new Man("Ares", 28, Sex.MALE, null);
+        Man[] mans = {originalMan, manTwo};
+
+        Man[] copy = (Man[]) copyUtils.deepCopy(mans);
+
+        Man man = mans[1];
+        man.setName("Hermes");
+        man.setAge(33);
+        man.setSex(Sex.FEMALE);
+        man.setFavoriteBooks(null);
+
+        assertNotEquals(mans, copy);
+
+        System.out.println("Original array " + Arrays.toString(mans));
+        System.out.println("Deep Copy array " + Arrays.toString(copy));
+    }
+
+ /*   @Test
+    @DisplayName("Test Deep Copy Collection Of Strings")
+    void deepCopyCollectionOfPrimitivesTest(){
+        Map<Integer, Integer> originalList = new HashMap<>();
+        originalList.put(0, 1);
+        originalList.put(1, 2);
+        Map copy = (Map) copyUtils.deepCopy(originalList);
+
+        originalList.put(2, 3);
+
+        assertNotEquals(copy, originalList);
+
+        System.out.println("Original collection " + originalList);
+        System.out.println("DeepCopy collection " + copy);
+    }*/
+
+    @Test
+    @DisplayName("Test Deep Copy Collection Of Strings")
+    void deepCopyCollectionOfStringsTest(){
+        Map<Integer,String> originalList = new HashMap<>();
+        originalList.put(1, "How I do it?");
+        originalList.put(2, "It is working");
+
+        Map copy = (Map) copyUtils.deepCopy(originalList);
+
+        originalList.put(3, "Testing String");
+
+        assertNotEquals(copy, originalList);
+
+        System.out.println("Original collection " + originalList);
+        System.out.println("DeepCopy collection " + copy);
+    }
+
+    @Test
+    @DisplayName("Test Deep Copy Collection Of Objects")
+    void deepCopyCollectionOfObjectsTest(){
+        List<Man> originalList = new ArrayList<>();
+        Man manTwo = new Man("Ares", 28, Sex.MALE, null);
+
+        originalList.add(originalMan);
+        originalList.add(manTwo);
+
+        List copy = (List) copyUtils.deepCopy(originalList);
+
+        manTwo.setName("Hermes");
+        manTwo.setAge(33);
+        manTwo.setSex(Sex.FEMALE);
+        manTwo.setFavoriteBooks(null);
+
+        assertNotEquals(copy, originalList);
+
+        System.out.println("Original collection " + originalList);
+        System.out.println("DeepCopy collection " + copy);
+    }
+
+    @Test
     @DisplayName("Test Deep Copy Primitive Field Object")
     void deepCopyPrimitiveFieldObject() {
-
         Man copy = (Man) copyUtils.deepCopy(originalMan);
 
         originalMan.setAge(28);
 
         assertNotEquals(copy.getAge(), originalMan.getAge());
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Deep Copy String Field Object")
     void deepCopyStringFieldObject() {
-
         Man copy = (Man) copyUtils.deepCopy(originalMan);
 
         originalMan.setName("Ares");
 
         assertNotEquals(copy.getName(), originalMan.getName());
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Deep Copy Enum Field Object")
     void deepCopyEnumFieldObject() {
-
         Man copy = (Man) copyUtils.deepCopy(originalMan);
 
         originalMan.setSex(Sex.MALE);
 
         assertNotEquals(copy.getSex(), originalMan.getSex());
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Deep Copy Collection Field Object")
     void deepCopyCollectionFieldObject() {
-
         Man copy = (Man) copyUtils.deepCopy(originalMan);
 
         originalMan.getFavoriteBooks().add("myths and reality");
 
         assertNotEquals(copy.getFavoriteBooks(), originalMan.getFavoriteBooks());
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Deep Copy Object Field Object")
     void deepCopyObjectFieldObject() {
-
         Man copy = (Man) copyUtils.deepCopy(originalMan);
 
         originalMan.setName("Ares");
@@ -93,48 +205,62 @@ class CopyUtilsTest {
         originalMan.getFavoriteBooks().add("myths and reality");
 
         assertNotEquals(copy, originalMan);
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Field Array Of Primitives Deep Copy")
     void deepCopyFieldArrayOfPrimitivesTest() {
-        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(original);
+        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(originalArray);
 
-        original.setTestPrimitiveArray(new int[]{4, 5, 6});
+        originalArray.setTestPrimitiveArray(new int[]{4, 5, 6});
 
-        assertNotEquals(copy, original);
+        assertNotEquals(copy, originalArray);
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Field Array Of Strings Deep Copy")
     void deepCopyFieldArrayOfStringsTest() {
-        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(original);
+        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(originalArray);
 
-        original.getTestStringArray()[0] = "Воздух";
-        original.getTestStringArray()[1] = "Огонь";
+        originalArray.getTestStringArray()[0] = "Воздух";
+        originalArray.getTestStringArray()[1] = "Огонь";
 
-        assertNotEquals(copy, original);
+        assertNotEquals(copy, originalArray);
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Field Array Of Objects Deep Copy")
     void deepCopyFieldArrayOfObjectsTest() {
-        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(original);
+        ObjectWithArrays copy = (ObjectWithArrays) copyUtils.deepCopy(originalArray);
 
-        Man man = (Man) original.getTestObjectArray()[1];
+        Man man = (Man) originalArray.getTestObjectArray()[1];
         man.setName("Hermes");
         man.setAge(33);
         man.setSex(Sex.FEMALE);
         man.setFavoriteBooks(null);
 
-        assertNotEquals(copy, original);
+        assertNotEquals(copy, originalArray);
+
+        System.out.println("Original object " + originalMan);
+        System.out.println("DeepCopy object " + copy);
     }
 
     @Test
     @DisplayName("Test Cached Exception")
     void throwsExceptionsTest() {
         originalMan = null;
+
         assertThrows(IllegalArgumentException.class, () -> {
+
             copyUtils.deepCopy(originalMan);
         });
     }
